62b62d41ab61378600ed8676ac237baa95041e2b
Lionel <lpei1900@gmail.com>
2024-06-24 22:29:18 -0400

    -Created basic layout -Implemented the object constructor -Created form that creates object

---
+++ script.js
@@ -0,0 +1,38 @@
+let library = [];
+
+
+function Book(title, author, pages, read = false) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
+}
+
+
+//Adds a book to the screen
+function addBookToLibrary(bookObject) {
+    //To do later
+}
+
+function outputAllBooks() {
+    //To do later
+}
+
+const bookForm = document.getElementById('bookForm');
+
+bookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    handleFormSubmit();
+});
+
+function handleFormSubmit (){
+    let titleInput = document.getElementById('title').value;
+    let authorInput = document.getElementById('author').value;
+    let pagesInput = parseInt(document.getElementById('pages').value);
+    let isReadInput = document.getElementById('isRead').checked;
+    const newBook = new Book(titleInput, authorInput, pagesInput, isReadInput);
+    library.push(newBook);
+    addBookToLibrary(newBook);
+    console.log(library);
+    console.log(newBook);
+}
